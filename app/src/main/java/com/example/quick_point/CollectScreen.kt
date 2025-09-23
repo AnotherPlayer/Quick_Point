38f1a4b5a89fd53ef7c9589a61d04d9b1e9ea568
Another_One <hd.tetuan5002@gmail.com>
2025-09-23 08:42:10 -0600

    Add collect + pay

---
+++ app/src/main/java/com/example/quick_point/CollectScreen.kt
@@ -0,0 +1,26 @@
+package com.example.quick_point
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun CollectScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Pantalla de Cobrar",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
